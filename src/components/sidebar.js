bf5d78651aa21f3e17c63b2f42ea87d8a1b15c73
ivopavlov <pavlov.ivo@gmail.com>
2020-02-17 23:35:14 -0800

    styling updates with sticky

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -5,23 +5,23 @@ const Sidebar = ({productData}) => {
 
     return (
       <div className="sidebar">
-        <img
-          className="product-image"
-          src={`${productData.image}`}
-          alt={`${productData.subtitle}`}
-        ></img>
-        <h2 className="sidebar-title">{productData.title}</h2>
-        <h3 className="sidebar-subtitle">{productData.subtitle}</h3>
-        <div className="tags">
-          {productData.tags.map(tag => (
-            <h5 className="sidebar-tag">{tag}</h5>
-          ))}
-        </div>
-        <div className="display-buttons">
-          <button className="btn">OVERVIEW</button>
-          <button className="btn">
-            SALES
-          </button>
+        <div className="sidebar-stick">
+          <img
+            className="product-image"
+            src={`${productData.image}`}
+            alt={`${productData.subtitle}`}
+          ></img>
+          <h2 className="sidebar-title">{productData.title}</h2>
+          <h3 className="sidebar-subtitle">{productData.subtitle}</h3>
+          <div className="tags">
+            {productData.tags.map(tag => (
+              <h5 className="sidebar-tag">{tag}</h5>
+            ))}
+          </div>
+          <div className="display-buttons">
+            <button className="btn">OVERVIEW</button>
+            <button className="btn">SALES</button>
+          </div>
         </div>
       </div>
     );
